f5256ced94fb3b7fc4bad6cf2f8ce8966a8f085e
Ubuntu <ubuntu@ip-172-31-33-246.us-east-2.compute.internal>
2018-11-12 12:05:35 +0000

    edit deploy

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ set -e
 echo ""
 echo -e "SSHing to Kubeadm machine... and executing kubectl"
 ssh -i /home/ubuntu/key -o StrictHostKeyChecking=no ubuntu@18.218.172.238 "cd /home/ubuntu/code/kubectl-ssh && git pull https://github.com/sahil-sharma/kubectl-ssh.git"
-kubectl create -f /home/ubuntu/code/kubectl-ssh
+kubectl create -f /home/ubuntu/code/kubectl-ssh/yaml-files/
