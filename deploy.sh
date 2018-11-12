3a308f0d48f4b9244d0f41f24b2ad8eed6e7e59b
Ubuntu <ubuntu@ip-172-31-33-246.us-east-2.compute.internal>
2018-11-12 12:02:06 +0000

    edit deploy

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 set -e
 echo ""
 echo -e "SSHing to Kubeadm machine... and executing kubectl"
-ssh -i /home/ubuntu/key -o StrictHostKeyChecking=no ubuntu@18.218.172.238 "bash /home/ubuntu/start.sh"
-#ssh -i /home/ubuntu/key -o StrictHostKeyChecking=no ubuntu@18.218.172.238 "cd /home/ubuntu/ && git pull https://github.com/sahil-sharma/k8s-demo-app.git"
+ssh -i /home/ubuntu/key -o StrictHostKeyChecking=no ubuntu@18.218.172.238 "cd /home/ubuntu/code/kubectl-ssh && git pull https://github.com/sahil-sharma/kubectl-ssh.git"
+kubectl create -f /home/ubuntu/code/kubectl-ssh
