e90eecbdad1aa118b8cc1b0f56d865a7d14f6d8f
Ubuntu <ubuntu@ip-172-31-33-246.us-east-2.compute.internal>
2018-11-13 06:11:36 +0000

    add if statement

--- yaml-files/start.sh
+++ yaml-files/start.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #set -e
 echo ""
-echo -e "Pulling changes from the repo..."
 cd /home/ubuntu/code/kubectl-ssh/yaml-files
 echo ""
 echo -e "List of running deployments"
 deployment_name=`kubectl get deploy -o name -n=appnamespace`
 echo $deployment_name
+if [ -z "$deployment_name" ]; then
+   echo "Sorry no deployment so far..."
+   exit
+fi
 sleep 3
 echo ""
 echo -e "Executing YAML file..."
@@ -14,8 +17,15 @@ kubectl apply -f $HOME/new-files/namespace.yaml
 echo ""
 kubectl apply -f $HOME/new-files/deployment.yaml --record=true -f $HOME/new-files/service.yaml
 echo ""
-kubectl rollout status $deployment_name -n=appnamespace
-echo ""
-kubectl rollout history $deployment_name -n=appnamespace
-echo ""
-kubectl get deploy -o name -n=appnamespace
+if [ -z "$deployment_name" ]; then
+   #echo "There is no such deployment for this application"
+   exit
+else
+   kubectl rollout status $deployment_name -n=appnamespace
+   echo ""
+   kubectl rollout history $deployment_name -n=appnamespace
+   echo ""
+   echo -e "Final set of running deployments are as... :"
+   kubectl get deploy -o name -n=appnamespace
+   echo ""
+fi
