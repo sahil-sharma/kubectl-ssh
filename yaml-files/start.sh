e3576e7c83edb04d46bfe7ee2a235fe70c6f44ca
Ubuntu <ubuntu@ip-172-31-33-246.us-east-2.compute.internal>
2018-11-14 10:24:14 +0000

    change dev

--- yaml-files/start.sh
+++ yaml-files/start.sh
@@ -2,7 +2,7 @@
 #set -e
 cd $HOME/kubectl-ssh/yaml-files
 echo -e "List of running deployments"
-deployment_name=`kubectl get deploy -o name -n=dev`
+deployment_name=`kubectl get deploy -o name -n=development`
 echo $deployment_name
 if [ -z "$deployment_name" ]; then
    echo "Sorry no deployment so far..."
@@ -18,14 +18,14 @@ sleep 3
 kubectl apply -f $HOME/kubectl-ssh/yaml-files/deployment.yaml --record=true -f $HOME/kubectl-ssh/yaml-files/service.yaml -f $HOME/kubectl-ssh/yaml-files/configmap.yaml
 sleep 3
 echo ""
-deployment_name=`kubectl get deploy -o name -n=dev`
-kubectl rollout status $deployment_name -n=dev
+deployment_name=`kubectl get deploy -o name -n=development`
+kubectl rollout status $deployment_name -n=development
 echo ""
-kubectl rollout history $deployment_name -n=dev
+kubectl rollout history $deployment_name -n=development
 echo ""
 echo -e "Final set of running deployment and pod are as..."
-kubectl get deploy -o name -n=dev
+kubectl get deploy -o name -n=development
 echo ""
-kubectl get po -o name -n=dev
+kubectl get po -o name -n=development
 sleep 3
 
